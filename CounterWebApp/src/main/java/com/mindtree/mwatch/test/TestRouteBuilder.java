cee2a787b1f132cfafe3e26d5aaee20c35b169cf
lakshmi <lakshmi.ramamoorthy19@gmail.com>
2016-11-15 17:54:42 +0530

    Added Rest service

--- CounterWebApp/src/main/java/com/mindtree/mwatch/test/TestRouteBuilder.java
+++ CounterWebApp/src/main/java/com/mindtree/mwatch/test/TestRouteBuilder.java
@@ -1,10 +1,11 @@
 package com.mindtree.mwatch.test;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestRouteBuilder  extends RouteBuilder{
 	
+
 	public TestRouteBuilder() {
 		
 		System.out.println(">>Object created");
@@ -13,10 +14,16 @@ public class TestRouteBuilder  extends RouteBuilder{
 	@Override
 	public void configure() throws Exception {
 		
-			System.out.println( ">> Builder called");
-			from("restlet:/hello")
-		 .transform().constant("Hello World");
+		System.out.println( ">> Builder called");
+		from("restlet:/hello").transform().constant("Hello World");
+		
+		from("restlet:/user/{userId}?restletMethods=GET").bean(Service.class, "getUserDetails");
       
+	}
+	
+	public void customerDetail()
+	{
+		System.out.println("Customer Detail");
 	}
 
 }
